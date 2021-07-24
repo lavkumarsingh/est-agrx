fdc7e219529897bd08bd477418730ab114d83cba
lavkumarsingh <lavksingh68495@gmail.com>
2021-07-24 13:02:29 +0530

    'nav'

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+/* import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close'; */
 import { 
   BrowserRouter as Router, 
   Route, 
@@ -14,43 +16,53 @@ import Technology from '../../pages/technology/technology'
 import Partners from '../../pages/partners/partners'
 
 function Nav() {
+    const [toggle, setToggle] = useState(false)
     return (
-    <Router>
-        <nav>
-          {/* LOGO */}
-          <div className='logo'>AGRX</div>
-
-          <div className='list_container'>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='technology'>Technology</Link>
-            </li>
-            <li>
-              <Link to='partners'>Partners</Link>
-            </li>
+        <div>
+          <div className='side_nav'>
+{/*             toggle  ? <CloseIcon style={{color: "#fff", fontSize: 30}} />
+                    : <MenuIcon style={{color: "#fff", fontSize: 30}} /> */}
           </div>
-        </nav>
+      
+          <Router>
+              <div className=''>
+              <nav>
+                {/* LOGO */}
+                <div className='logo'>AGRX</div>
 
-        <Switch>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/technology'>
-            <Technology />
-          </Route>
-          <Route path='/partners'>
-            <Partners />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+                <div className='list_container'>
+                  <li>
+                    <Link to='/'>Home</Link>
+                  </li>
+                  <li>
+                    <Link to='/about'>About</Link>
+                  </li>
+                  <li>
+                    <Link to='technology'>Technology</Link>
+                  </li>
+                  <li>
+                    <Link to='partners'>Partners</Link>
+                  </li>
+                </div>
+              </nav>
+              </div>
+
+              <Switch>
+                <Route path='/about'>
+                  <About />
+                </Route>
+                <Route path='/technology'>
+                  <Technology />
+                </Route>
+                <Route path='/partners'>
+                  <Partners />
+                </Route>
+                <Route path='/'>
+                  <Home />
+                </Route>
+              </Switch>
+          </Router>
+        </div>
     )
 }
 
