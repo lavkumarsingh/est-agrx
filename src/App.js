9820e8d765b50395579a90363ac6d9c9649d70f1
lavkumarsingh <lavksingh68495@gmail.com>
2021-07-30 01:34:54 +0530

    'audits'

--- src/App.js
+++ src/App.js
@@ -48,30 +48,51 @@ function App() {
                       <li>
                         <Link to='partners'>Partners</Link>
                       </li>
+                      <li>
+                        <div className='button_top'>Login</div>
+                      </li>
                     </div>
 
 
-                    {toggle ? <div className='menu_icon' onClick={handleClick}><img src='/icons8-delete-60.png' alt='' /></div>
-                            : <div className='menu_icon' onClick={handleClick}><img src='/icons8-menu-50.png' alt='' /></div>}
+                    {toggle ? <div className='menu_icon' onClick={handleClick}><span class="material-icons">close</span></div>
+                            : <div className='menu_icon' onClick={handleClick}><span class="material-icons">menu</span></div>}
                     <div className={`side_nav  ${toggle ? 'nav_active': ''}`}>
                     <div className='side'>
-                      <div>
+                      <div className='routes'>
                         <li>
+                          <span class="material-icons">
+                            home
+                          </span>
                           <Link onClick={focus} to='/'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 40" fill="none" x="0px" y="0px"><path d="M32 30V13.7613C32 13.1537 31.7238 12.5791 31.2494 12.1995L17.2494 0.999512C16.519 0.415162 15.481 0.415163 14.7506 0.999513L0.75061 12.1995C0.276179 12.5791 0 13.1537 0 13.7613V30C0 31.1046 0.89543 32 2 32H9.73333C10.8379 32 11.7333 31.1046 11.7333 30V22.2667C11.7333 21.1621 12.6288 20.2667 13.7333 20.2667H18.2667C19.3712 20.2667 20.2667 21.1621 20.2667 22.2667V30C20.2667 31.1046 21.1621 32 22.2667 32H30C31.1046 32 32 31.1046 32 30Z" fill="black"/><text x="0" y="47" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">Created by Maru</text><text x="0" y="52" fill="#000000" font-size="5px" font-weight="bold" font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">from the Noun Project</text></svg>
                             Home
                           </Link>
                         </li>
                         <li>
-                          <Link onClick={focus} to='/about'>About</Link>
+                          <span class="material-icons">
+                            text_rotation_none
+                          </span>
+                          <Link onClick={focus} to='/about'>
+                            About
+                          </Link>
                         </li>
                         <li>
-                          <Link onClick={focus} to='technology'>Technology</Link>
+                          <span class="material-icons">
+                            biotech
+                          </span>
+                          <Link onClick={focus} to='technology'>
+                            Technology
+                          </Link>
                         </li>
                         <li>
-                          <Link onClick={focus} to='partners'>Partners</Link>
+                          <span class="material-icons">
+                            group_work
+                          </span>
+                          <Link onClick={focus} to='partners'>
+                            Partners
+                          </Link>
                         </li>
                       </div>
+                      <div className='button_side'>Login</div>
                     </div>
                     </div>
                   </nav>
